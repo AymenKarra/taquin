eded6f3964c038ee680fd85f793ee8003877b015
AymenKarra <76628712+AymenKarra@users.noreply.github.com>
2021-04-07 20:48:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<SFML/Graphics.hpp>
+#include<SFML/Audio.hpp>
 #include<iostream>
 #include"GameManager.h"
 
@@ -6,6 +7,14 @@ using namespace std;
 using namespace sf;
 
 int main() {
+	Music music;
+	Music woow;
+	music.openFromFile("Spanish Flea (Herb Albert) - Comedy Background Music (HD).ogg");
+	music.setLoop(true);
+	SoundBuffer buffer;
+	buffer.loadFromFile("Roblox Death Sound - Sound Effect (HD)-[AudioTrimmer.com].ogg");
+	Sound sound;
+	sound.setBuffer(buffer);
 	cout << "enter the level you want to play, levels 3,4,5" << endl;
 	int n;
 	cin >> n;
@@ -15,7 +24,7 @@ int main() {
 	}
 	GameMaster game(n);
 	RenderWindow app(VideoMode(91*n, 91*n), "TAQUIN");
-	app.setFramerateLimit(10);
+	app.setFramerateLimit(60);
 	Texture t;
 	t.loadFromFile("images/numbers.png");
 	int w = 91;
@@ -24,8 +33,7 @@ int main() {
 		sprite[i+1].setTexture(t);
 		sprite[i+1].setTextureRect(IntRect(i*w, 0, w, w));
 	}
-
-
+	music.play();
 	while (app.isOpen()) {
         Event e;
         while (app.pollEvent(e))
@@ -41,7 +49,7 @@ int main() {
                     int y = pos.y / w ;
 
 					game.move(y, x);
-                    
+					sound.play();
                 }
 
         }
@@ -53,10 +61,14 @@ int main() {
 				sprite[num].setPosition(j * w, i * w);
 				app.draw(sprite[num]);
 			}
-
 		app.display();
 		if (game.GameOver()) {
-			cout << "winner" << endl;
+			woow.openFromFile("Anime wow sound effect.ogg");
+			music.stop();
+			woow.play();
+			cout << "w";
+			for (int i = 0;i < 50000;i++) cout<<"o";
+			cout << "w" << endl;
 			app.close();
 		}
 	}
